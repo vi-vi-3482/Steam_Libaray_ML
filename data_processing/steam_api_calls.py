fcee5a9dcf4cac22b3cb05a67010c0f64c740b9b
ViViViViViViViViViViViVIVi <78184297+ViViViViViViViViViViViVIVi@users.noreply.github.com>
2023-02-23 12:44:25 -0800

    Added steam user id as field to import from json.

--- data_processing/steam_api_calls.py
+++ data_processing/steam_api_calls.py
@@ -1,7 +1,8 @@
 import json
 
 with open(r"apikey.json") as f:
-    api_key = json.load(f)
-    api_key = api_key["steam_api_key"]
+    api_userid = json.load(f)
 
+api_key = api_userid["steam_api_key"]
+user_id = api_userid["user_id"]
 
