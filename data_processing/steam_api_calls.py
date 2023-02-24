5865cf1c1a70dc8af1ad3b458f841d258c253c06
ViViViViViViViViViViViVIVi <78184297+ViViViViViViViViViViViVIVi@users.noreply.github.com>
2023-02-24 12:39:21 -0800

    Added docstrings to functions.
    Started game_summary() function to gather relevent data of a game into one dict. This will later include completion time. Genre list will be expanded into true false fields for each genre.

--- data_processing/steam_api_calls.py
+++ data_processing/steam_api_calls.py
@@ -35,11 +35,13 @@ def get_library(user_id: str, api=api) -> list:
     return user_library
 
 
-def get_game_info(game_id):
+def get_game_info(game_id: str) -> dict:
     """
-
+    Gets details for a steam game from the storefront api. Returns a json as a dict.
     :param game_id:
-    :return:
+    :type: str
+    :return: game_info
+    :type: dict
     """
     with urllib.request.urlopen(fr"https://store.steampowered.com/api/appdetails?appids={game_id}") as url:
         game_info = json.load(url)
@@ -47,25 +49,58 @@ def get_game_info(game_id):
     return game_info
 
 
-def get_review_info(game_id):
-
+def get_review_info(game_id: str) -> dict:
+    """
+    Gets review info from steam storefront api, removes review text and returns a dict.
+    :param game_id:
+    :type: str
+    :return review_info:
+    :type: dict
+    """
     with urllib.request.urlopen(fr"https://store.steampowered.com/appreviews/{game_id}?json=1&language=all") as url:
         response = json.load(url)
 
     review_info = response["query_summary"]
-
 
     pprint.pprint(review_info)
     return review_info
 
 
 def review_ratio(review_dict):
+    """
+    Calculates ratio of positive reviews to negative reviews from a game, returns a float between 0 and 1.
+
+    :param review_dict:
+    :type: dict
+    :return:
+    :type: float
+    """
     positive = review_dict["total_positive"]
     negative = review_dict["total_negative"]
 
     ratio = positive / (positive + negative)
 
     return ratio
+
+
+def game_summary(game_id, play_time):
+    info = get_game_info(game_id)
+    score = get_review_info(game_id)
+
+    game_name = info[str(game_id)]["data"]["name"]
+
+    genres_dict = info[str(game_id)]["data"]["genres"]
+    genre_list = [v["description"] for k, v in genres_dict.items()]
+
+    review_score = review_ratio(score)
+
+    summary = {
+        "game_id": str(game_id),
+        "genres": genre_list,
+        "review_score": review_score  # TODO expand into T/F fields for each possible genre
+    }
+
+    return summary
 
 
 if __name__ == "__main__":
