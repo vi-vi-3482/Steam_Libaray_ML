014ed89cc3228c27adcda4e6edb3cefd034d42c6
ViViViViViViViViViViViVIVi <78184297+ViViViViViViViViViViViVIVi@users.noreply.github.com>
2023-02-24 11:35:31 -0800

    Minor changes to docustring.
    Added get_game_info() using the storefront api to poll directly for steam storefront json.

--- data_processing/steam_api_calls.py
+++ data_processing/steam_api_calls.py
@@ -1,6 +1,7 @@
 import json
 import steam.webapi as steam_api
 import pprint
+import urllib.request
 
 with open(r"apikey.json") as f:
     api_userid = json.load(f)
@@ -11,12 +12,13 @@ user_id = api_userid["user_id"]
 api = steam_api.WebAPI(key=api_key)
 
 
-def get_library(api, user_id: str) -> list:
+def get_library(user_id: str, api=api) -> list:
     """
     Query's steams web api and returns a list of all games tied to a user id as a list of dictionaries.
-    :param api: steamapi object
+
     :param user_id: user id from s://steamcommunity.com/profiles/{USER_ID}/
     :type: str
+    :param api: steamapi object
     :return: list of all game dictionaries
     :type: list
     """
@@ -33,7 +35,21 @@ def get_library(api, user_id: str) -> list:
     return user_library
 
 
+def get_game_info(game_id, api=api):
+    """
+
+    :param game_id:
+    :param api:
+    :return:
+    """
+    with urllib.request.urlopen(fr"https://store.steampowered.com/api/appdetails?appids={game_id}") as url:
+        game_info = json.load(url)
+    pprint.pprint(game_info)
+    return game_info
+
 user_library = get_library(api, user_id)
 
-pprint.pprint(user_library)
+info = get_game_info(440)
+
+# pprint.pprint(user_library)
 print("done")
